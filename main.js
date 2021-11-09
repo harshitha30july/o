ea8e5d4de8749e72072bba23be9cf95b27a38c43
harshitha30july <82657326+harshitha30july@users.noreply.github.com>
2021-11-09 18:03:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{ 
+
+}
+
+function setup()
+{
+    var canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+
+ function draw()
+ {
+
+ }
+
+ function clear_canvas()
+ {
+     background("white");
+ }
